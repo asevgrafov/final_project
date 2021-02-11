ae1320b277768131977a57322790f07aac50072a
stanger <4blokadanhilla@gmail.com>
2021-02-11 16:29:35 +0300

    add_base

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,10 +1,12 @@
+from common.base import BaseClass
+from common.constants import PersonalData
 from locators.checkout import CheckoutLocators
 import logging
 
 logger = logging.getLogger()
 
 
-class CheckoutPage:
+class CheckoutPage(BaseClass):
     def __init__(self, app):
         self.app = app
 
@@ -20,21 +22,30 @@ class CheckoutPage:
 
     def input_firstname(self, firstname):
         logger.info(f"Пытаемся заполнить поле firstname значением {firstname}")
-        self.firstname().send_keys(firstname)
+        self.input_value(self.firstname(), PersonalData.FIRSTNAME)
 
     def lastname(self):
         return self.app.driver.find_element(*CheckoutLocators.LASTNAME)
 
     def input_lastname(self, lastname):
         logger.info(f"Пытаемся заполнить поле lastname значением {lastname}")
-        self.lastname().send_keys(lastname)
+        self.input_value(self.lastname(), PersonalData.LASTNAME)
 
     def postal_code(self):
         return self.app.driver.find_element(*CheckoutLocators.POSTAL_CODE)
 
     def input_postal_code(self, postal_code):
         logger.info(f"Пытаемся заполнить поле postal code значением {postal_code}")
-        self.postal_code().send_keys(postal_code)
+        self.input_value(self.postal_code(), PersonalData.POSTAL_CODE)
+
+    def input_all_value(self, firstname, lastname, postal_code):
+        logger.info(
+            f"Пытаемся заполнить поля значениями: "
+            f"{firstname}, {lastname}, {postal_code}"
+        )
+        self.input_value(self.firstname(), PersonalData.FIRSTNAME)
+        self.input_value(self.lastname(), PersonalData.LASTNAME)
+        self.input_value(self.postal_code(), PersonalData.POSTAL_CODE)
 
     def cancel(self):
         return self.app.driver.find_element(*CheckoutLocators.CANCEL)
